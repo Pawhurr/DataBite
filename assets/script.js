a8089f8fc262d10483a3e9adaef0d6bcac9bec82
mcnamarakc <mcnamarakc@users.noreply.github.com>
2019-04-17 10:34:11 -0400

    resolve search bar location and function

--- assets/script.js
+++ assets/script.js
@@ -102,8 +102,6 @@ var storeUserGroceryList = {
 storeUserGroceryList.listItem = list
 console.log(storeUserGroceryList)
 database.ref().push(storeUserGroceryList);
-    
-};
 
 $("#add-groceries").on("click", function(event){
     event.preventDefault();
@@ -197,7 +195,7 @@ function initAutocomplete() {
   // Create the search box and link it to the UI element.
   var input = document.getElementById('pac-input');
   var searchBox = new google.maps.places.SearchBox(input);
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
   // Bias the SearchBox results towards current map's viewport.
   map.addListener('bounds_changed', function() {
