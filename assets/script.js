f6b6b6383ebdafe05dd66608f56ddd27d3e74b91
mcnamarakc <mcnamarakc@users.noreply.github.com>
2019-04-18 12:35:52 -0400

    fancified search bar in maps

--- assets/script.js
+++ assets/script.js
@@ -197,9 +197,14 @@ function initAutocomplete() {
   });
 
   // Create the search box and link it to the UI element.
-  var input = document.getElementById('pac-input');
+  var input = document.getElementById('zipCodeInput');
+  var options = {
+    types: "supermarket",
+  };
   var searchBox = new google.maps.places.SearchBox(input);
   // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  
 
   // Bias the SearchBox results towards current map's viewport.
   map.addListener('bounds_changed', function() {
